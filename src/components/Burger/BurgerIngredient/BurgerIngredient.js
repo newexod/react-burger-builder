fbd3eb2295851f824e2c72c081aec87aa189f8b8
newexod <dimapulenko@gmail.com>
2017-12-06 01:24:12 +0300

    Starting the Burger Component

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import propTypes from 'prop-types';
 
 import classes from './BurgerIngredient.css';
 
@@ -37,15 +37,15 @@ class BurgerIngredient extends Component {
         ingredient = <div className={classes.Salad}></div>;
         break;
   
-      default = null;
+      default : null;
     }
   
     return ingredient;
   }
 }
 
-BurgerIngredient.PropTypes = {
-  type: PropTypes.string.isRequired
+BurgerIngredient.propTypes = {
+  type: propTypes.string.isRequired
 };
 
 export default BurgerIngredient;
