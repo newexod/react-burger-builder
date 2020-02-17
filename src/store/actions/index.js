0ff20e329274ee1d76d2d97eac01abac2ef8187c
dpulenko <dpulenko@gbsfo.com>
2020-02-17 17:17:50 +0200

    Moving the Orders Side Effects into Sagas

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,3 +1,3 @@
 export { addIngredient, removeIngredient, initIngredients, setIngredients, fetchIngredientsFailed } from './burgerBuilder';
-export { purchaseBurger, purchaseInit, fetchOrders } from './order';
+export { purchaseBurger, purchaseInit, fetchOrders, purchaseBurgerStart, purchaseBurgerSuccess, purchaseBurgerFail, fetchOrdersStart, fetchOrdersSuccess, fetchOrdersFail } from './order';
 export { auth, logout, setAuthRedirectPath, authCheckState, logoutSucceed, authStart, authSuccess, checkAuthTimeout, authFail } from './auth';
