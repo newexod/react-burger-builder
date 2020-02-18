adfd5a599adcf7424a2b18f4addcddb14a0ced88
dpulenko <dpulenko@gbsfo.com>
2020-02-18 14:35:52 +0200

    Using React.memo() & More

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,37 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import ReactAux from '../../../hoc/ReactAux/ReactAux';
 import Button from '../../UI/Button/Button';
 
-class OrderSummary extends Component {
-   render () {
-    const ingredientSummary = Object.keys(this.props.ingredients)
-    .map(igKey => {
-      return (
-        <li key={igKey}>
-          <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
-        </li>
-      ); 
-    });
-
+const orderSummary = props => {
+  const ingredientSummary = Object.keys(props.ingredients)
+  .map(igKey => {
     return (
-      <ReactAux>
-        <h3>Your Order</h3>
-        <p>A delicious burger with the following ingredients:</p>
-        <ul>
-          {ingredientSummary}
-        </ul>
-        <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
-        <p>Continue to Checkout?</p>
-        <Button 
-          btnType="Danger"
-          clicked={this.props.purchaseCancelled}>CANCEL</Button>
-        <Button 
-          btnType="Success"
-          clicked={this.props.purchaseContinued}>CONTINUE</Button>
-      </ReactAux>
-    );
-  }
+      <li key={igKey}>
+        <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+      </li>
+    ); 
+  });
+
+  return (
+    <ReactAux>
+      <h3>Your Order</h3>
+      <p>A delicious burger with the following ingredients:</p>
+      <ul>
+        {ingredientSummary}
+      </ul>
+      <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
+      <p>Continue to Checkout?</p>
+      <Button 
+        btnType="Danger"
+        clicked={props.purchaseCancelled}>CANCEL</Button>
+      <Button 
+        btnType="Success"
+        clicked={props.purchaseContinued}>CONTINUE</Button>
+    </ReactAux>
+  );
 };
 
-export default OrderSummary;
+export default orderSummary;
